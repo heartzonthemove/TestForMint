245087db981408be9fa3d8fcfb4a11e9e1f59534
CAP.NON <taewan.thebigboss@mail.kmutt.ac.th>
2020-09-18 03:06:06 +0700

    Add files via upload

---
+++ TestForMint/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TestForMint                                                                                           // set up repository
+{
+    class Program                                                                                               // setup class  
+    {
+        static void Main(string[] args)                                                                         // start of void
+        {
+            static double recur(int n, int UserInput)                                                           // recursion function 
+            {                                                                                                   // start of recursion function 
+                if (n == UserInput+1)                                                                           // if condition
+                {                                                                                               // start if
+                    return 2 * n - 1;                                                                           // loop cause
+                }                                                                                               // end if
+                else
+                {                                                                                               // start else
+                    return (2 * n) - 1 + (Math.Pow(n, 2) / recur(n + 1, UserInput));                            // continue fraction function (do some math test)
+                }                                                                                               // end else
+            }                                                                                                   // end of recursion function 
+
+            Console.Write("Input Limit n : ");                                                                  // ask for user input
+            int UserInput = int.Parse(Console.ReadLine());                                                      // waiting for the command
+
+            if (UserInput > 0)                                                                                  // if condition (start if user input is more than zero)
+            {                                                                                                   // start if
+                Console.WriteLine("Estimate value of PI is {0}", 4 / recur(1, UserInput));                      // print the PI value
+            }                                                                                                   // end if
+
+            else
+            {                                                                                                   // start else
+                Console.WriteLine("Can not estimate value of PI.");                                             // print error (print to not print)
+            }                                                                                                   // end else
+
+            Console.ReadLine();                                                                                 // this line code is for safety in the visual studio program
+        }                                                                                                       // end of void
+    }                                                                                                           // end of class  
+}                                                                                                               // end of repository
+//////////////////////////////////////////////////////////////////////////////end of code//////////////////////////////////////////////////////////////////////////////
